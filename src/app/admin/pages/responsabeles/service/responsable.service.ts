d8c9bcde9938db4ff256795df0caf1f165e08e5c
karima ROUIN <karimarouin.ing@gmail.com>
2021-06-11 09:34:14 +0100

    add methode for get and setheader

--- src/app/admin/pages/responsabeles/service/responsable.service.ts
+++ src/app/admin/pages/responsabeles/service/responsable.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { api } from 'src/app/Api/api';
+import { responsable } from '../model/responsable';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +10,19 @@ export class ResponsableService {
 
   constructor(private http: HttpClient) { }
   customersUrl=api.url+'responsable/all';
+  
+  public  getallResponsableData() {
+  
+    return  this.http.get<responsable[]>(this.customersUrl);
+    }
+
+    setHttpHeaders(token: string) {
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Access-Control-Allow-Origin':'*',
+          'Authorization': 'Bearer ' + token.split('"').join('')
+        })
+      };
+      return httpOptions;
+    }
 }
