a6793c83dc4d16cf5d864c0259ab89a8a497e03f
karima ROUIN <karimarouin.ing@gmail.com>
2021-06-16 17:49:17 +0100

    update formulaire add device

--- src/app/admin/pages/devices/adddevice/adddevice.component.ts
+++ src/app/admin/pages/devices/adddevice/adddevice.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import custom validator to validate that password and confirm password fields match
+import { MustMatch } from '../../../../_helpers/must-match.validator';
 @Component({
   selector: 'app-adddevice',
   templateUrl: './adddevice.component.html',
   styleUrls: ['./adddevice.component.scss']
 })
 export class AdddeviceComponent implements OnInit {
+  registerForm!: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+      this.registerForm = this.formBuilder.group({
+         // title: ['', Validators.required],
+         reference: ['', Validators.required],
+         name: ['', Validators.required],
+          // validates date format yyyy-mm-dd
+         // dob: ['', [Validators.required, Validators.pattern(/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/)]],
+         kiosk: ['', Validators.required]
+          //acceptTerms: [false, Validators.requiredTrue]
+      });
   }
 
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.registerForm.invalid) {
+
+        console.log("registerForm.invalid");
+          return;
+      }
+      console.log('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+ 
+      // display form values on success
+      alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+  }
+
+  onReset() {
+      this.submitted = false;
+      this.registerForm.reset();
+  }
 }
--- src/app/admin/pages/kiosks/addkiosk/addkiosk.component.ts
+++ src/app/admin/pages/kiosks/addkiosk/addkiosk.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import custom validator to validate that password and confirm password fields match
+import { MustMatch } from '../../../../_helpers/must-match.validator';
 @Component({
   selector: 'app-addkiosk',
   templateUrl: './addkiosk.component.html',
   styleUrls: ['./addkiosk.component.scss']
 })
 export class AddkioskComponent implements OnInit {
+  registerForm!: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+      this.registerForm = this.formBuilder.group({
+         // title: ['', Validators.required],
+          id: ['', Validators.required],
+          name: ['', Validators.required],
+          // validates date format yyyy-mm-dd
+         // dob: ['', [Validators.required, Validators.pattern(/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/)]],
+         adresse: ['', Validators.required],
+         responsable: ['', Validators.required]
+          //acceptTerms: [false, Validators.requiredTrue]
+      });
   }
 
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.registerForm.invalid) {
+
+        console.log("registerForm.invalid");
+          return;
+      }
+      console.log('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+ 
+      // display form values on success
+      alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+  }
+
+  onReset() {
+      this.submitted = false;
+      this.registerForm.reset();
+  }
 }
